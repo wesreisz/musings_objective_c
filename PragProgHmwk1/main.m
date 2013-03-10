50b4555d1ea62e9248b2982a9014ac34916b2c8f
Wesley Reisz <wes@wesleyreisz.com>
2013-03-10 10:52:38 -0400

    Initial Commit

---
+++ PragProgHmwk1/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  PragProgHmwk1
+//
+//  Created by Wesley Reisz on 3/10/13.
+//  Copyright (c) 2013 com.wesleyreisz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
